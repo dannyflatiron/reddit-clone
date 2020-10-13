f1bf24befabd1ab90b832a7592bbe3d9849cd5de
dannyflatiron <dannyflatiron@gmail.com>
2020-10-12 22:32:43 -0400

    feat: created route for profile

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :accounts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  get "u/:username" => "public#profile", as: :profile
+
   resources :communities do 
     resources :posts
   end
