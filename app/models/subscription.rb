5169204d9036a8829600ce1d81fc434f0e154a6b
dannyflatiron <dannyflatiron@gmail.com>
2020-10-13 09:55:30 -0400

    feat: create helper method finds subscriber

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,8 @@
 class Subscription < ApplicationRecord
     belongs_to :account
     belongs_to :community
+
+    def self.check_if_account_signed_in(community, current_account)
+        Subscription.where(community_id: community.id, account_id: current_account.id).any?
+    end
 end
