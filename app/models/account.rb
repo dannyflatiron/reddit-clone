f324f3941c70aaac29bffd7bfdbb7a7ac7b0b025
dannyflatiron <dannyflatiron@gmail.com>
2020-10-12 22:29:49 -0400

    feat: validate presence of first name last name and username

--- app/models/account.rb
+++ app/models/account.rb
@@ -7,6 +7,8 @@ class Account < ApplicationRecord
   has_many :communities
   has_many :posts
 
+  validates_presence_of :first_name, :last_name, :username
+
   def full_name
     "#{first_name} #{last_name}"
   end
