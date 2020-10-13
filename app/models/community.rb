fd3be53a48f047cc1bc432e16c4664ab72d6d804
dannyflatiron <dannyflatiron@gmail.com>
2020-10-12 23:20:00 -0400

    feat: community model has many subscriptions and many accounts through subscriptions

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,6 +1,8 @@
 class Community < ApplicationRecord
     belongs_to :account
     has_many :posts
+    has_many :subscriptions
+    has_many :accounts, through: :subscriptions
     validates_presence_of :url, :name, :rules
 
 
