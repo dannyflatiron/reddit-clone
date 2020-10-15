4a0328008edda0935dc0dbd928148893844ae209
dannyflatiron <dannyflatiron@gmail.com>
2020-10-15 16:13:23 -0400

    feat: added comments to a post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
     belongs_to :account
     belongs_to :community
+    has_many :comments
     validates_presence_of :title, :body, :account_id, :community_id
-
 
     def self.post_order
         order(id: :desc).limit(20)
