ddcef0e8b69c1e01480a463e74256014d82de079
dannyflatiron <dannyflatiron@gmail.com>
2020-10-15 16:09:26 -0400

    feat: find comments that belong to current post id. Fix typo in comment_params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,15 @@
 class CommentsController < ApplicationController
 
     def create
-        @comment = Comment.new comment_params
+        @comment = Comment.new(comment_params)
         @comment.account_id = current_account.id
 
         respond_to do |format|
             format.js {
                 if @comment.save
-                    render "messages/create"
+                    @comments = Comment.where(post_id: @comment.post_id)
+
+                    render "comments/create"
                 else
                 end
             }
@@ -17,6 +19,6 @@ class CommentsController < ApplicationController
     private
 
     def comment_params
-        params.require(:comnment).permit(:message, :post_id)
+        params.require(:comment).permit(:message, :post_id)
     end
 end
