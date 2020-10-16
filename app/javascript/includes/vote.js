90b40e516305691fe6c10c136a6db126f6ab7dee
dannyflatiron <dannyflatiron@gmail.com>
2020-10-16 02:02:46 -0400

    feat: require vote.js when compiled

--- app/javascript/includes/vote.js
+++ app/javascript/includes/vote.js
@@ -1,3 +1,18 @@
 $(function() {
   console.log("jquery is loaded")
+
+  $(".vote").on("click", ".upvote, .downvote", function(){
+    var post_id = $(this).parent().data("id"),
+      is_upvote = $(this).hasClass("upvote")
+    console.log("post_id", post_id)
+    console.log("is_upvote", is_upvote)
+    $.ajax({
+      url: "/post/vote",
+      method: "POST",
+      data: { post_id: post_id, upvote: is_upvote },
+      success: function(){
+        console.log("success")
+      }
+    })
+  })
 })
