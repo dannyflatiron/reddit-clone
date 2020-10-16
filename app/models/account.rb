1fcd3f2dc1bc9833769bf44705d7e034dfff167a
dannyflatiron <dannyflatiron@gmail.com>
2020-10-16 02:16:29 -0400

    feat: add votes to account. fix active state for arrows

--- app/models/account.rb
+++ app/models/account.rb
@@ -8,10 +8,19 @@ class Account < ApplicationRecord
   has_many :communities, through: :subscriptions
   has_many :posts
   has_many :comments
+  has_many :votes
 
   validates_presence_of :first_name, :last_name, :username
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def upvoted_post_ids
+    ids = self.votes.where(upvote: true).pluck(:post_id)
+  end
+
+  def downvoted_post_ids
+    self.votes.where(upvote: false).pluck(:post_id)
   end
 end
