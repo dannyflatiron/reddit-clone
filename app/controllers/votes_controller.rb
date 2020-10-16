d6fe888b622b0a483a6613787d48166b2809e439
dannyflatiron <dannyflatiron@gmail.com>
2020-10-15 21:42:35 -0400

    fix: testing is required

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,36 @@
+class VotesController < ApplicationController
+
+    def create
+        post_id = params[:vote][:post_id]
+        vote = Vote.new(vote_params)
+        vote.account_id = current_account.account_id
+
+        existing_vote = Vote.where(account_id: current_account.id, post_id: post_id)
+
+        respond_to do |format|
+            format.js {
+                if existing_vote.size > 0
+                    exisiting_vote.first.destroy
+                else
+                    if vote.save
+                        @success = true
+                    else
+                        @success = false
+                    end
+
+                    @post = Post.find(post_id)
+                    @total_upvotes = @post.upvotes
+                    @total_downvotes = @post.downvotes
+                end
+                render "votes/create"
+            }
+        end
+    end
+
+    private
+
+    def vote_params
+        params.require(:vote).permit(:upvote, :post_id)
+    end
+
+end
