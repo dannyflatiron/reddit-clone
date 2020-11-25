260e4f7e8ba18bcf183045a88ee5c85f87204261
dannyflatiron <dannyflatiron@gmail.com>
2020-11-25 04:51:23 -0500

    feat: add has_many votes association. total_comments returns numbers of comments a post has

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
     belongs_to :account
     belongs_to :community
     has_many :comments
+    has_many :votes
     validates_presence_of :title, :body, :account_id, :community_id
 
     def self.post_order
@@ -12,6 +13,14 @@ class Post < ApplicationRecord
     def score
         if self.upvotes > 0 || self.downvotes > 0
             self.upvotes > 0 ? (self.upvotes - self.downvotes) : (self.downvotes * -1)
+        else
+            0
+        end
+    end
+
+    def total_comments
+        if self.comments
+            self.comments.count
         else
             0
         end
